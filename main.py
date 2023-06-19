849059331ef4bdd51bd30d8650af81b67f883714
sakibtamim <sakib.tamim@gmail.com>
2023-06-20 03:25:21 +0600

    final commit

--- main.py
+++ main.py
@@ -2,47 +2,53 @@ import random
 
 guesses_list = []
 
-number = random.randint(1, 10)
-guesses = 0
 
-print('Hello! Welcome to the game of guesses!')
-player_name = input('What is your name? ')
-wanna_play = input(
-    f'Hi, {player_name}, would you like to play '
-    f'the guessing game? (Enter Yes/No): ')
+def start_game():
+    number = random.randint(1, 10)
+    guesses = 0
 
-if wanna_play != 'yes':
-    print('That\'s cool, Thanks!')
-    exit()
-else:
-    pass
+    print('Hello! Welcome to the game of guesses!')
+    player_name = input('What is your name? ')
+    wanna_play = input(
+        f'Hi, {player_name}, would you like to play '
+        f'the guessing game? (Enter Yes/No): ')
 
-while wanna_play == 'yes':
-    try:
-        guess = int(input("Enter Guess: "))
+    if wanna_play != 'yes':
+        print('That\'s cool, Thanks!')
+        exit()
+    else:
+        pass
 
-        guesses += 1
-        guesses_list.append(guesses)
+    while wanna_play == 'yes':
+        try:
+            guess = int(input("Enter Guess: "))
 
-        if guess == number:
-            print('Nice! You got it.')
-            print(f'it took you {guesses} guesses')
-            wanna_play = input(
-                f'Hi, {player_name}, would you like to play '
-                f'the guessing game? (Enter Yes/No): ')
-            if wanna_play != 'yes':
-                print(f'That\'s cool, Thanks!')
-                break
+            guesses += 1
+            guesses_list.append(guesses)
+
+            if guess == number:
+                print('Nice! You got it.')
+                print(f'it took you {guesses} guesses')
+                wanna_play = input(
+                    f'Hi, {player_name}, would you like to play '
+                    f'the guessing game? (Enter Yes/No): ')
+                if wanna_play != 'yes':
+                    print(f'That\'s cool, Thanks!')
+                    break
+                else:
+                    guesses = 0
+                    number = random.randint(1, 10)
+                    continue
+
             else:
-                guesses = 0
-                number = random.randint(1, 10)
-                continue
+                if guess < number:
+                    print("Guess higher!")
+                elif guess > number:
+                    print("Guess lower!")
 
-        else:
-            if guess < number:
-                print("Guess higher!")
-            elif guess > number:
-                print("Guess lower!")
+        except ValueError:
+            print('That is not a valid value. Try again...')
 
-    except ValueError as error:
-        print('That is not a valid value. Try again...')
+
+if __name__ == '__main__':
+    start_game()
